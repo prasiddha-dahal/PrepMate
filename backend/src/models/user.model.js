ceb3fa8473fa316ad4b3d06b0a9d676d0ce16e92
prasiddha <prasiddhadahal33@gmail.com>
2026-05-03 13:43:45 +0545

    chore: user model setup

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: [true, "username already taken"],
+        required: true
+    },
+    email:{
+        type: String,
+        unique: [true, "account already exists with this email address"],
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+},{ timestamps: true })
+
+
+module.exports = mongoose.model('user', userSchema)

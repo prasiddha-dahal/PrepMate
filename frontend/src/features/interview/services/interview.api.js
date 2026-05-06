693a12053a96a31ff83eee6352e2cb1c3dd52c51
prasiddha <prasiddhadahal33@gmail.com>
2026-05-06 15:45:39 +0545

    feat: implement the api for interview folder

--- frontend/src/features/interview/services/interview.api.js
+++ frontend/src/features/interview/services/interview.api.js
@@ -4,3 +4,34 @@ const api = axios.create({
     baseURL: "http://localhost:3000/",
     withCredentials: true
 })
+
+export const generateInteviewReport = async({ selfDescription, jobDescription, resume }) => {
+
+    const formData = new FormData();
+    formData.append("jobDescription", jobDescription)
+    formData.append("selfDescription", selfDescription)
+    formData.append("resume", resume)
+
+    const response = await api.post("/api/interview", formData, {
+        headers: {
+            "Content-Type": "multipart/form-data"
+        }
+    })
+
+    return response.data
+}
+
+export const getInterviewReportById = async(interviewId) => {
+
+    const response = await api.get(`/api/interview/report/${interviewId}`);
+
+    return response.data
+}
+
+
+export const getAllInterviewsReports = async() => {
+
+    const response = await api.get('/api/interview');
+
+    return response.data
+}
